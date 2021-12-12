4493a31459567ecca9d076438394fe66b5a656e7
zkindest <zkindest5@gmail.com>
2021-12-12 17:48:44 +0530

    remove unnecessary comments

--- src/scripts/theme.ts
+++ src/scripts/theme.ts
@@ -1,27 +1,5 @@
 import { THEME_VARS, INITIAL_COLOR_MODE_CSS_PROP, COLOR_MODE_KEY } from '../constants/theme'
 
-// const THEME_VARS = {
-//   'clr-text': {
-//     light: 'hsl(200,15%,8%)',
-//     dark: 'hsl(0,0%,100%)'
-//   },
-//   'clr-elements': {
-//     light: 'hsl(0,0%,100%)',
-//     dark: 'hsl(209, 23%, 22%)'
-//   },
-//   'clr-input': {
-//     light: 'hsl(0,0%,52%)',
-//     dark: 'hsl(0, 0%, 100%)'
-//   },
-//   'clr-bg': {
-//     light: 'hsl(0,0%,98%)',
-//     dark: 'hsl(207, 26%, 17%)'
-//   },
-//   'shadow-color': {
-//     light: '280deg 6% 76%',
-//     dark: '0deg 0% 17%'
-//   }
-// }
 (function setTheme() {
   let colorMode: 'light' | 'dark' = 'light';
   const persistedColorPreference = window.localStorage.getItem(COLOR_MODE_KEY);
